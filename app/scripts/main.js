6bb178bdee863c0fd1b7df7c1d6ce032081c8b5f
Danny Summerlin <danny.summerlin@jstart.org>
2021-11-22 17:48:53 -0500

    more pruning, adding simple menu parser

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,6 @@
 // @copyright 2012+ Daniel Nakov / Silverline CRM
 // http://silverlinecrm.com
 // @copyright 2019+ Danny Summerlin
-// var orgId = null
-// var userId = null
-// var sessionId = null
-
 let sessionHash = null
 let serverInstance = '' // figure out if this is the same for every tenant
 let apiUrl = '' // make this optional for Intacct
@@ -22,13 +18,10 @@ const openInIFrame = (url)=>{
 	else
 		backupOpenInIFrame('iamain', url)
 }
-const getSessionID = ()=>{
-	return window.location.search.match(/\.sess=(.*)&*/)[1]
-}
 
 var intacctNavigator = (()=>{
 	function loadCommands(force) {
-		if(serverInstance == null || orgId == null || sessionId == null) { init(); return false }
+		if(serverInstance == null || sessionId == null) { init(); return false }
 		commands['Refresh Metadata'] = {}
 		commands['Merge Accounts'] = {}
 		commands['Setup'] = {}
@@ -38,21 +31,23 @@ var intacctNavigator = (()=>{
 			sessionHash: sessionHash,
 			apiUrl: apiUrl
 		}
-		chrome.runtime.sendMessage(Object.assign(options, {
-				action:'getSetupTree'
-		}), response=>{
-			Object.assign(commands, response)
-		})
-		chrome.runtime.sendMessage(Object.assign(options, {
-			action:'getMetadata'
-		}), response=>{
-			Object.assign(commands, response)
-		})
-		chrome.runtime.sendMessage(Object.assign(options, {
-			action:'getCustomObjects'
-		}), response=>{
-			Object.assign(commands, response)
-		})
+		let menu = []
+		try {
+			[...document.getElementsByClassName('qx-siamenu-main')[0].getElementsByTagName('a')].forEach(a=>{
+				if(a.text.trim() != '') {
+					menu.push({label: a.text.trim(), link: a.href})
+				}
+			})
+			Object.assign(commands, menu)
+		} catch(e) {
+			console.log(e)
+		}
+		// Likely won't need this because we'll use on page parsing instead
+		// chrome.runtime.sendMessage(Object.assign(options, {
+		// 	action:'getCustomObjects'
+		// }), response=>{
+		// 	Object.assign(commands, response)
+		// })
 		hideLoadingIndicator()
 	}
 	function invokeCommand(cmd, newTab, event) {
@@ -66,12 +61,6 @@ var intacctNavigator = (()=>{
 				document.getElementById("nav_quickSearch").value = ""
 				return true
 				break
-			// case "toggle lightning":
-			// 	let mode
-			// 	if(window.location.href.includes("lightning.force")) mode = "classic"
-			// 	else mode = "lex-campaign"
-			// 	targetUrl = serverInstance + "/ltng/switcher?destination=" + mode
-			// 	break
 			case "setup":
 				targetUrl = serverInstance + "/setup"
 				break
@@ -79,10 +68,7 @@ var intacctNavigator = (()=>{
 				targetUrl = serverInstance + "/"
 				break
 		}
-		if(checkCmd.substring(0,9) == 'login as ') { loginAs(cmd, newTab); return true }
-		else if(checkCmd.substring(0,14) == "merge accounts") { launchMergerAccounts(cmd.substring(14).trim()) }
-		// else if(checkCmd.substring(0,11) == "merge cases") { launchMergerCases(cmd.substring(11).trim()) } //TODO more complicated merge call, will make later
-		else if(checkCmd.substring(0,1) == "!") { createTask(cmd.substring(1).trim()) }
+		// else if(checkCmd.substring(0,1) == "!") { createTask(cmd.substring(1).trim()) }
 		else if(checkCmd.substring(0,1) == "?") { targetUrl = searchTerms(cmd.substring(1).trim()) }
 		else if(typeof commands[cmd] != 'undefined' && commands[cmd].url) { targetUrl = commands[cmd].url }
 		else if(debug && !checkCmd.includes("create a task: !") && !checkCmd.includes("global search usage")) {
@@ -117,114 +103,6 @@ var intacctNavigator = (()=>{
 		cb.remove()
 		return clipboardValue
 	}
-	// var getIdFromUrl = ()=>{
-	// 	const url = document.location.href
-	// 	const ID_RE = [
-	// 		/http[s]?\:\/\/.*force\.com\/.*([a-zA-Z0-9]{18})[^\w]*/, // tries to find the first 18 digit
-	// 		/http[s]?\:\/\/.*force\.com\/.*([a-zA-Z0-9]{15})[^\w]*/ // falls back to 15 digit
-	// 	]
-	// 	for(var i in ID_RE) {
-	// 		var match = url.match(ID_RE[i])
-	// 		if (match != null) { return match[1] }
-	// 	}
-	// 	return false
-	// }
-	// var launchMerger = function(otherId, object) {
-	// 	if(!otherId)
-	// 		otherId = pasteFromClipboard()
-	// 	if(![15,18].includes(otherId.length)) {
-	// 		clearOutput()
-	// 		addWord('[ERROR: you must have an Account ID in your clipboard or type one after the merge command]')
-	// 		return
-	// 	}
-	// 	const thisId = getIdFromUrl()
-	// 	if(thisId)
-	// 		switch(object) {
-	// 			case 'Account':
-	// 				document.location.href = `${serverInstance}/merge/accmergewizard.jsp?goNext=+Next+&cid=${otherId}&cid=${thisId}`
-	// 				break
-	// 			case 'Case':
-	// 				//TODO - needs to be a post request, so fetch or background will have to happen here
-	// 				/*
-	// 				const options = {
-	// 					method: 'POST',
-	// 					body: `{"message": {
-	// 						"actions": [{
-	// 							"id":"2319;a",
-	// 							"descriptor":"serviceComponent://ui.merge.components.controller.MergeController/ACTION$loadMergeComparisonData",
-	// 							"callingDescriptor":"UNKNOWN",
-	// 							"params":{"recordIds":[otherId,thisId]}
-	// 						}]
-	// 					}}`
-	// 				}
-	// 					fetch(serverInstance+"/aura?r=23&ui-merge-components-controller.Merge.loadMergeComparisonData=1",options)
-	// 				*/
-	// 				break
-	// 		}
-	// }
-	// var launchMergerAccounts = (otherId)=>launchMerger(otherId, 'Account')
-	// var launchMergerCases = (otherId)=>launchMerger(otherId, 'Case')
-	// var createTask = function(subject) {
-	// 	showLoadingIndicator()
-	// 	if(subject != "" && userId) {
-	// 		chrome.runtime.sendMessage({
-	// 				action:'createTask', apiUrl: apiUrl,
-	// 				key: sessionHash, sessionId: sessionId,
-	// 				domain: serverInstance, sessionHash: sessionHash,
-	// 				subject: subject, userId: userId
-	// 			}, response=>{
-	// 			if(response.errors.length == 0) {
-	// 				clearOutput()
-	// 				commands["Go To Created Task"] = {url: serverInstance + "/"+ response.id }
-	// 				document.getElementById("nav_quickSearch").value = ""
-	// 				addWord('Go To Created Task')
-	// 				addWord('(press escape to exit or enter a new command)')
-	// 				let firstEl = document.querySelector('#nav_output :first-child')
-	// 				if(listPosition == -1 && firstEl != null)
-	// 					firstEl.className = "nav_child nav_selected"
-	// 				hideLoadingIndicator()
-	// 			}
-	// 		})
-	// 	}
-	// }
-	// function loginAs(cmd, newTab) {
-	// 	let cmdSplit = cmd.split(' ')
-	// 	let searchValue = cmdSplit[2]
-	// 	if(cmdSplit[3] !== undefined)
-	// 		searchValue += '+' + cmdSplit[3]
-	// 	showLoadingIndicator()
-	// 	chrome.runtime.sendMessage({
-	// 		action:'searchLogins', apiUrl: apiUrl,
-	// 		key: sessionHash, sessionId: sessionId,
-	// 		domain: serverInstance, sessionHash: sessionHash,
-	// 		searchValue: searchValue, userId: userId
-	// 	}, success=>{
-	// 		let numberOfUserRecords = success.records.length
-	// 		hideLoadingIndicator()
-	// 		if(numberOfUserRecords < 1) { addError([{"message":"No user for your search exists."}]) }
-	// 		else if(numberOfUserRecords > 1) { loginAsShowOptions(success.records) }
-	// 		else {
-	// 			var userId = success.records[0].Id
-	// 			loginAsPerform(userId, newTab)
-	// 		}
-	// 	})
-	// }
-	// function loginAsShowOptions(records) {
-	// 	for(let i = 0; i < records.length; ++i) {
-	// 		let cmd = 'Login As ' + records[i].Name
-	// 		commands[cmd] = {key: cmd, id: records[i].Id}
-	// 		addWord(cmd)
-	// 	}
-	// 	let firstEl = document.querySelector('#nav_output :first-child')
-	// 	if(listPosition == -1 && firstEl != null) firstEl.className = "nav_child nav_selected"
-	// }
-	// function loginAsPerform(userId, newTab) {
-	// 	let targetUrl = "https://" + apiUrl + "/servlet/servlet.su?oid=" + orgId + "&suorgadminid=" + userId + "&retURL=" + encodeURIComponent(window.location.pathname) + "&targetURL=" + encodeURIComponent(window.location.pathname) + "&"
-	// 	hideSearchBox()
-	// 	if(newTab) goToUrl(targetUrl, true)
-	// 	else goToUrl(targetUrl)
-	// 	return true
-	// }
 
 // ================================================================================================
 // Interaction handling
@@ -280,16 +158,16 @@ var intacctNavigator = (()=>{
 
 // ===============================================================================================
 // Interface
-	function showLoadingIndicator() { document.getElementById('nav_loader').style.visibility = 'visible' }
-	function hideLoadingIndicator() { document.getElementById('nav_loader').style.visibility = 'hidden' }
-	var hideSearchBox = function() {
+	const showLoadingIndicator = ()=>{ document.getElementById('nav_loader').style.visibility = 'visible' }
+	const hideLoadingIndicator = ()=>{ document.getElementById('nav_loader').style.visibility = 'hidden' }
+	const hideSearchBox = ()=>{
 		let searchBar = document.getElementById('nav_quickSearch')
 		searchBar.blur()
 		clearOutput()
 		searchBar.value = ''
 		setVisibleSearch("hidden")
 	}
-	function setVisibleSearch(visibility) {
+	const setVisibleSearch = (visibility)=>{
 		let searchBox = document.getElementById("nav_searchBox")
 		if(visibility == "hidden") {
 			searchBox.style.opacity = 0
@@ -301,7 +179,7 @@ var intacctNavigator = (()=>{
 			document.getElementById("nav_quickSearch").focus()
 		}
 	}
-	function lookAt() {
+	const lookAt = ()=>{
 		let newSearchVal = document.getElementById('nav_quickSearch').value
 		if(newSearchVal !== '') addElements(newSearchVal)
 		else {
@@ -309,10 +187,10 @@ var intacctNavigator = (()=>{
 			listPosition = -1
 		}
 	}
-	function addElements(input) {
+	const addElements = (input)=>{
 		clearOutput()
 		if(input.substring(0,1) == "?") addWord('Global Search Usage: ? <Search term(s)>')
-		else if(input.substring(0,1) == "!") addWord('Create a Task: ! <Subject line>')
+		// else if(input.substring(0,1) == "!") addWord('Create a Task: ! <Subject line>')
 		else {
 			let words = getWord(input, commands)
 			if(words.length > 0)
@@ -321,11 +199,10 @@ var intacctNavigator = (()=>{
 			else
 			listPosition = -1
 		}
-		if ('login as'.includes(input.toLowerCase())) addWord('Usage: login as <FirstName> <LastName> OR <Username>')
 		let firstEl = document.querySelector('#nav_output :first-child')
 		if(listPosition == -1 && firstEl != null) firstEl.className = "nav_child nav_selected"
 	}
-	var getWord = function(input, dict) {
+	const getWord = (input, dict)=>{
 		if(typeof input === 'undefined' || input == '') return []
 		let foundCommands = [],
 			dictItems = [],
@@ -351,9 +228,9 @@ var intacctNavigator = (()=>{
 			foundCommands.push(dictItems[i].key)
 		return foundCommands
 	} 
-	function addWord(word) {
-		var d = document.createElement("div")
-		var sp
+	const addWord = (word)=>{
+		let d = document.createElement("div")
+		let sp
 		if(commands[word] != null && commands[word].url != null && commands[word].url != "") {
 			sp = document.createElement("a")
 			if(commands[word].url.startsWith('//'))
@@ -369,7 +246,7 @@ var intacctNavigator = (()=>{
 		if(sp.id && sp.length > 0) { sp.onclick = mouseClickLoginAs }
 		searchBox.appendChild(sp)
 	}
-	function addError(text) {
+	const addError = (text)=>{
 		clearOutput()
 		let err = document.createElement("div")
 		err.className = "nav_child nav-error-wrapper"
@@ -381,8 +258,8 @@ var intacctNavigator = (()=>{
 		}
 		searchBox.appendChild(err)
 	}
-	function clearOutput() { if(typeof searchBox != 'undefined') searchBox.innerHTML = "" }
-	function kbdCommand(e, key) {
+	const clearOutput = ()=>{ if(typeof searchBox != 'undefined') searchBox.innerHTML = "" }
+	const kbdCommand = (e, key)=>{
 		let position = listPosition
 		let origText = '', newText = ''
 		if(position < 0) position = 0
@@ -428,9 +305,9 @@ var intacctNavigator = (()=>{
 		try {
 			document.onkeyup = (ev)=>{ window.ctrlKey = ev.ctrlKey }
 			document.onkeydown = (ev)=>{ window.ctrlKey = ev.ctrlKey }
-			orgId = document.cookie.match(/sid=([\w\d]+)/)[1]
 			serverInstance = getServerInstance()
 			sessionHash = getSessionHash()
+
 			if(sessionId == null) {
 				chrome.runtime.sendMessage({ action: 'getApiSessionId', key: orgId }, response=>{
 					if(response.error) console.log("response", orgId, response, chrome.runtime.lastError)
@@ -441,7 +318,7 @@ var intacctNavigator = (()=>{
 						var div = document.createElement('div')
 						div.setAttribute('id', 'nav_searchBox')
 						var loaderURL = chrome.extension.getURL("images/ajax-loader.gif")
-						var logoURL = chrome.extension.getURL("images/sf-navigator128.png")
+						var logoURL = chrome.extension.getURL("images/navigator128.png")
 						div.innerHTML = `
 <div class="nav_wrapper">
 	<input type="text" id="nav_quickSearch" autocomplete="off"/>
--- app/scripts/shared.js
+++ app/scripts/shared.js
@@ -1,30 +1,13 @@
-// var getServerInstance = ()=>{
-// 	let targetUrl
-// 	let url = location.origin + ""
-// 	if(url.indexOf("lightning.force") != -1)
-// 		targetUrl = url.substring(0, url.indexOf("lightning.force")) + "lightning.force.com"
-// 	else if(url.indexOf("salesforce") != -1)
-// 		targetUrl = url.substring(0, url.indexOf("salesforce")) + "salesforce.com"
-// 	else if(url.indexOf("cloudforce") != -1)
-// 		targetUrl = url.substring(0, url.indexOf("cloudforce")) + "cloudforce.com"
-// 	else if(url.indexOf("visual.force") != -1) {
-// 		let urlParseArray = url.split(".")
-// 		targetUrl = 'https://' + urlParseArray[1] + ''
-// 	}
-// 	return targetUrl
-// }
-var getSessionHash = ()=>{
-	try {
-		let sId = document.cookie.match(regMatchSid)[1]
-		return sId.split('!')[0] + '!' + sId.substring(sId.length - 10, sId.length)
-	} catch(e) { if(debug) console.log(e) }
+const getSessionID = ()=>{
+	// do I need to check anything in document.cookie?
+	return window.location.search.match(/\.sess=(.*)&*/)[1]
 }
+
 let getHTTP = function(targetUrl, type = "json", headers = {}, data = {}, method = "GET") {
 	let request = { method: method, headers: headers }
 	if(Object.keys(data).length > 0)
 		request.body = JSON.stringify(data)
 	return fetch(targetUrl, request).then(response => {
-		apiUrl = response.url.match(/:\/\/(.*)salesforce.com/)[1] + "salesforce.com"
 		switch(type) {
 			case "json": return response.clone().json()
 			case "document": return response.clone().text()
@@ -37,25 +20,8 @@ let getHTTP = function(targetUrl, type = "json", headers = {}, data = {}, method
 	})
 }
 
-
 const debug = false
 const newTabKeys = [ "ctrl+enter", "command+enter", "shift+enter" ]
-const regMatchSid = /sid=([a-zA-Z0-9\.\!]+)/
-const SFAPI_VERSION = 'v49.0'
-// const classicToLightingMap = {
-// 	'Fields': "/FieldsAndRelationships/view",
-// 	'Page Layouts': '/PageLayouts/view',
-// 	'Lightning Record Pages': '/LightningPages/view',
-// 	'Buttons, Links, and Actions': '/ButtonsLinksActions/view',
-// 	'Compact Layouts': '/CompactLayouts/view',
-// 	'Field Sets': '/FieldSets/view',
-// 	'Limits': '/Limits/view',
-// 	'Record Types': '/RecordTypes/view',
-// 	'Related Lookup Filters': '/RelatedLookupFilters/view',
-// 	'Search Layouts': '/SearchLayouts/view',
-// 	'Triggers': '/Triggers/view',
-// 	'Validation Rules': '/ValidationRules/view'
-// }
 const setupLabelsToUrlMap = {
-// "Home": "/",
+	// "Home": "/",
 }
